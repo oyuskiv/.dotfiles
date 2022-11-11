d8352ee5e153403933ecbf97860e12d6ac766e7c
oyuskiv <44861514+oyuskiv@users.noreply.github.com>
2022-11-11 23:25:09 +0200

    telescope base setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-require('plugins')      -- plugin setup
-require('options')      -- common options setup
-require('filetype')     -- set options for certain file types
-require('colorscheme')  -- set colorscheme
+require('options') -- common options setup
+require('plugins') -- plugin setup
+require('filetype') -- set options for certain file types
+require('colorscheme') -- set colorscheme
--- nvim/lua/plugin-configs/telescope.lua
+++ nvim/lua/plugin-configs/telescope.lua
@@ -6,6 +6,33 @@ end
 
 telescope.setup {
     defaults = {
-        layout_config = { height = 0.90, width = 0.99 },
+        sorting_strategy = 'ascending',
+        layout_config = {
+            prompt_position = "top",
+            height = 0.90,
+            width = 0.95
+        },
     },
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        }
+    }
 }
+
+telescope.load_extension('fzf')
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<space>ff',
+    function()
+        builtin.find_files({ no_ignore = true, follow = true })
+    end,
+    {})
+vim.keymap.set('n', '<space>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<space>fs', builtin.grep_string, {})
+vim.keymap.set('n', '<space>fc', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<space>fb', builtin.buffers, {})
+vim.keymap.set('n', '<space>fd', builtin.lsp_document_symbols, {})
