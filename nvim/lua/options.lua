dae4d4290fc5c4d16effc346e5e74c55ba90143d
oyuskiv <44861514+oyuskiv@users.noreply.github.com>
2023-01-01 22:09:21 +0200

    vim: higlight on yank

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -43,3 +43,13 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     pattern = '*',
     group = group_autoformat
 })
+
+-- highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
