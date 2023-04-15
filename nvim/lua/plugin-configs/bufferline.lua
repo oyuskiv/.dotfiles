e9a8bbeeb3615dc6283d382b28c168ab63ba709f
oyuskiv <44861514+oyuskiv@users.noreply.github.com>
2023-04-15 20:48:28 +0300

    Nvim update (#9)
    * Update nvim: buferline, lua_lsp
    
    * bufferline: close other buffers
    
    * Update bufferline.lua

--- nvim/lua/plugin-configs/bufferline.lua
+++ nvim/lua/plugin-configs/bufferline.lua
@@ -16,5 +16,17 @@ vim.keymap.set('n', '<space>as', bufferline.pick_buffer, { desc = "Buffer: pick 
 vim.keymap.set('n', '<space>ac', bufferline.close_with_pick, { desc = "Buffer: close picked beffer" })
 vim.keymap.set('n', '<space>an', function() bufferline.cycle(1) end, { desc = "Buffer: next buffer" })
 vim.keymap.set('n', '<space>ap', function() bufferline.cycle(-1) end, { desc = "Buffer: previous beffer" })
-vim.keymap.set('n', '<space>ad', function() vim.cmd("wa|%bd|e#|bd#") end, { desc = "Buffer: close other buffers" })
+vim.keymap.set('n', '<space>ad',
+    function()
+        local active = vim.api.nvim_get_current_buf()
+        local buffers = vim.api.nvim_list_bufs()
+        for _, b in ipairs(buffers) do
+            if active ~= b then
+                vim.cmd("wa|bd" .. b)
+            end
+        end
+        vim.cmd("redrawtabline")
+        vim.cmd("redraw")
+    end,
+    { desc = "Buffer: close other buffers" })
 vim.keymap.set('n', '<space>aq', function() vim.cmd("cclose") end, { desc = "Buffer: close quick list" })
