ddfe11d882497a1a9a930c07076fb89b0fbd9cca
oyuskiv <44861514+oyuskiv@users.noreply.github.com>
2023-06-24 20:40:26 +0300

    lazy.nvim plugins manager

--- nvim/lua/plugin-configs/comment.lua
+++
@@ -1,7 +0,0 @@
-local ok, comment = pcall(require, 'Comment')
-if not ok then
-    vim.notify("Comment is not installed")
-    return
-end
-
-comment.setup()
---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,56 @@
+return {
+    {
+        'akinsho/bufferline.nvim',
+        version = '*',
+        dependencies = { { 'kyazdani42/nvim-web-devicons', lazy = false } },
+        lazy = false,
+        config = function()
+            local bufferline = require('bufferline')
+            bufferline.setup {
+                options = {
+                    show_close_icon = false,
+                    modified_icon = '',
+                    separator_style = 'thin',
+                    always_show_bufferline = true,
+                    custom_filter = function(buf_number, _)
+                        -- ignore quick fix list
+                        if vim.bo[buf_number].filetype ~= 'qf' then
+                            return true
+                        end
+                    end
+                }
+            }
+
+            vim.keymap.set('n', '<space>as', bufferline.pick_buffer, { desc = 'Buffer: pick buffer' })
+            vim.keymap.set('n', '<space>ac', bufferline.close_with_pick, { desc = 'Buffer: close picked beffer' })
+            vim.keymap.set('n', '<space>an', function() bufferline.cycle(1) end, { desc = 'Buffer: next buffer' })
+            vim.keymap.set('n', '<space>ap', function() bufferline.cycle(-1) end, { desc = 'Buffer: previous beffer' })
+            vim.keymap.set('n', '<space>ad',
+                function()
+                    vim.cmd('wa')
+                    local active = vim.api.nvim_get_current_buf()
+                    local buffers = vim.api.nvim_list_bufs()
+                    for _, b in ipairs(buffers) do
+                        if 1 == vim.fn.buflisted(b) then
+                            if active ~= b then
+                                vim.api.nvim_buf_delete(b, { force = true })
+                            end
+                        end
+                    end
+                    vim.cmd('redrawtabline')
+                    vim.cmd('redraw')
+                end,
+                { desc = 'Buffer: close other buffers' })
+            vim.keymap.set('n', '<space>aq',
+                function()
+                    local buffers = vim.api.nvim_list_bufs()
+                    for _, b in ipairs(buffers) do
+                        if vim.bo[b].filetype == 'qf' then
+                            vim.api.nvim_buf_delete(b, { force = true })
+                        end
+                    end
+                end,
+                { desc = 'Buffer: close quick list' })
+        end
+    }
+}
