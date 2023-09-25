3f6c15811e8bf12da2e87b761d21e5b0770e0bae
oyuskiv <44861514+oyuskiv@users.noreply.github.com>
2023-09-25 11:22:35 +0300

    feat: No expand tab for *.mk files (#52)

--- nvim/lua/filetype.lua
+++ nvim/lua/filetype.lua
@@ -2,7 +2,7 @@ local group_tab_indent = vim.api.nvim_create_augroup("tab indent", { clear = tru
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" },
   {
     command = "setlocal noexpandtab tabstop=4 shiftwidth=4",
-    pattern = { "*.go", "Makefile" },
+    pattern = { "*.go", "Makefile", "*.mk" },
     group = group_tab_indent,
   })
 local lua_indent = vim.api.nvim_create_augroup("lua indent", { clear = true })
