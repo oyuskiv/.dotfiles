895fe68d1de7cfd82f35aef6c7e13a73c7b76445
oyuskiv <44861514+oyuskiv@users.noreply.github.com>
2023-11-12 21:17:19 +0200

    feat: lf previewer

--- lf/previewer.sh
+++ lf/previewer.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 set +e
 
-if command -v pistol &> /dev/null
-then
-    export PISTOL_CHROMA_FORMATTER=terminal16m
-    export PISTOL_CHROMA_STYLE=nord
-    pistol "$@"
-else
-    bat -p --color always "$@"
-fi
+bat -p --color always "$@"
