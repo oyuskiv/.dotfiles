248d00c28127d21f7c3fe22a8210b2aa5c133acc
oyuskiv <44861514+oyuskiv@users.noreply.github.com>
2024-05-30 20:16:34 +0300

    feat: Set border for lsp preview window (nvim)

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -40,6 +40,11 @@ return {
         },
       })
 
+      -- Set border for floating preview window
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help,
+        { border = "rounded" })
+
       local diagnostic_state = true
       local navic = require('nvim-navic')
       -- Attach function of lsp servers
