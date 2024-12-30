ec820134d37578d3b35a28fabe017dd021ed1520
oyuskiv <44861514+oyuskiv@users.noreply.github.com>
2024-12-30 13:12:44 +0200

    feat: Add buf linter

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -10,6 +10,7 @@ return {
         sources = {
           null_ls.builtins.formatting.buf,
           null_ls.builtins.formatting.prettier,
+          null_ls.builtins.diagnostics.buf,
         },
       })
     end,
